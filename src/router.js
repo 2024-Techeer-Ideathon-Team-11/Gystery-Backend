36ef0338fc348f1969c5dd5f01b84790e0a92865
Won Chan Lee <twoone14@tukorea.ac.kr>
2024-06-01 00:50:58 +0900

    hint api 구현

--- src/router.js
+++ src/router.js
@@ -1,9 +1,11 @@
 import express from 'express';
 import quiz from "./api/quiz.js";
+import hint from "./api/hint.js";
 
 const router = express.Router();
 
 router.get('/quiz', quiz.home);
+router.get('/hint/:id', hint.home);
 // router.get('/param/:param', home.param);
 // router.post('/post', home.post);
 
